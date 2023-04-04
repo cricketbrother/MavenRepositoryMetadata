2e58abbdfa0522b1824af06c557078bec8e9b2c4
cricketbrother <zhouwenchao19870210@gmail.com>
2023-04-04 11:43:26 +0800

    fix errors

--- main.go
+++ main.go
@@ -1,11 +1,58 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
-	"time"
+	"io"
+	"net/http"
 )
 
+//type Catalog struct {
+//	XMLName    xml.Name    `xml:"archetype-catalog"`
+//	Archetypes []Archetype `xml:"archetypes>archetype"`
+//}
+
+type Archetype struct {
+	GroupID     string `xml:"groupId"`
+	ArtifactID  string `xml:"artifactId"`
+	Version     string `xml:"version"`
+	Description string `xml:"description"`
+}
+
+func GetArchetypes() {
+	resp, err := http.Get("https://repo1.maven.org/maven2/archetype-catalog.xml")
+	if err != nil {
+		panic(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	decoder := xml.NewDecoder(resp.Body)
+	count := 0
+	for {
+		token, err := decoder.Token()
+		if token == nil {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		if se, ok := token.(xml.StartElement); ok && se.Name.Local == "archetype" {
+			var archetype Archetype
+			if err := decoder.DecodeElement(&archetype, &se); err != nil {
+				panic(err)
+			}
+			fmt.Printf("%s, %s, %s, %s\n", archetype.GroupID, archetype.ArtifactID, archetype.Version, archetype.Description)
+			count += 1
+		}
+	}
+	fmt.Printf("Total: %d\n", count)
+}
+
 func main() {
-	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-	fmt.Println("Hello, world!")
+	GetArchetypes()
 }
